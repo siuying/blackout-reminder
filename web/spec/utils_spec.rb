d250de379f020229e7f79b5951a91c62bc7b08a9
siuying <siu.ying@gmail.com>
2011-03-17 01:13:04 +0800

    create task that generate json file

--- web/spec/utils_spec.rb
+++ web/spec/utils_spec.rb
@@ -5,8 +5,13 @@ require 'lib/blackout'
 
 describe Blackout::Utils do
   it "should return something" do
-    result = Blackout::Utils.file_list
+    result = Blackout::Utils.file_urls
     result.should have_at_least(1).items
     result.should have(9).items
+  end
+  
+  it "should provide time data" do
+    result = Blackout::Utils.blackout_time
+    result[:"1"].should have(2).items
   end
 end
