3c15129920f8839ef7c53715b3f2c3f35cd53ec5
Alex Hui <alex@ignition.hk>
2011-03-17 14:20:44 +0800

    Implemented nextBlackoutWithCurrentTime

--- Blackout/BlackoutUtils.m
+++ Blackout/BlackoutUtils.m
@@ -14,18 +14,36 @@
 +(BlackoutPeriod*) nextBlackoutWithCurrentTime:(NSDate*)currentTime
                                        periods:(NSArray*)periods {
     if ([periods count] > 0) {
-        // TODO really find out the next blackout
         
+        NSInteger numberOfPeriods = [periods count];
+        NSCalendar *gregorian = [[NSCalendar alloc]
+                                 initWithCalendarIdentifier:NSGregorianCalendar];
         
-        return [periods objectAtIndex:0];
+        int nextPeriod = 0;
+        
+        for (int i=0; i <numberOfPeriods; i++) {
+            
+            
+            BlackoutPeriod *period = [periods objectAtIndex:i];
+            NSDate *blackoutStart = [gregorian dateFromComponents:period.fromTime];
+            NSComparisonResult startResult = [currentTime compare:blackoutStart];
+            
+            //As long as start time of the period in the array is before current time, it is not the next blackout, so nextPeriod +1
+            if (startResult == NSOrderedDescending) {
+                nextPeriod += 1;
+            }
+        }
+        
+        return [periods objectAtIndex:nextPeriod];
+        
     } else {
+        
         return nil;
     }
 }
 
 +(BOOL) isBlackout:(NSDate*)currentTime
             period:(BlackoutPeriod*)period {
-    // TODO find if currently is blackout
     
     currentTime = [NSDate date];
     
@@ -40,6 +58,7 @@
     NSComparisonResult startResult = [currentTime compare:blackoutStart];
     NSComparisonResult endResult = [currentTime compare:blackoutEnd];
     
+    //Return YES if currentTime is within the blackout period. Otherwise return NO
     if (startResult == NSOrderedDescending && endResult == NSOrderedAscending) {
         
         return YES;
@@ -48,5 +67,7 @@
     
         return NO;
     }
+    
+    [gregorian release];
 }
 @end
